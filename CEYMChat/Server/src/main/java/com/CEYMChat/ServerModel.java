1ec8e044d5e8ed36b1b7ba510be4f60ee9b5f2f4
Darkarca <erik.gunnarsson98@gmail.com>
2018-09-25 12:11:36 +0200

    Servermodel added
    class created

---
+++ CEYMChat/Server/src/main/java/com/CEYMChat/ServerModel.java
@@ -0,0 +1,23 @@
+package com.CEYMChat.Model;
+
+import com.CEYMChat.Command;
+
+public class ServerModel {
+
+    public void logInUser(){
+
+    }
+
+    public void registerUser(){
+
+    }
+
+    public void performCommand(Command c){
+
+    }
+
+    public void startSession(){
+
+    }
+
+}
