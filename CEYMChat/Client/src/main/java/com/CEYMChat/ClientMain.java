ebfeab1b995a904eea10e99d0269b0dfcc59dc17
ostcarl <ostcarl@student.chalmers.se>
2018-10-01 15:23:44 +0200

    Made client runnable on all machines

--- CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
+++ CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * This class should do nothing else than to launch the client application.
@@ -18,7 +20,8 @@ public class ClientMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("View/ClientView.fxml"));
+        URL url = Paths.get("Client/src/main/resources/View/ClientView.fxml").toUri().toURL();
+        Parent root = FXMLLoader.load(url); root = FXMLLoader.load(getClass().getClassLoader().getResource("View/ClientView.fxml"));
         primaryStage.setTitle("CEYMChat");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
