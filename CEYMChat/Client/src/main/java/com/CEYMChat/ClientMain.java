89878f6e20358b5f851f6db6896643c96041b124
ostcarl <ostcarl@student.chalmers.se>
2018-10-01 10:36:06 +0200

    Fixed location of view

--- CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
+++ CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
@@ -3,6 +3,7 @@ package com.CEYMChat;
 
 import com.CEYMChat.Model.ClientModel;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,6 +11,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * This class should do nothing else than to launch the client application.
@@ -21,7 +24,8 @@ public class ClientMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         ClientModel model = ClientModel.getModelInstance();
-        Parent root = FXMLLoader.load(getClass().getResource("/com/CEYMChat/View/ClientView.fxml"));
+        URL url = Paths.get("Client/src/main/resources/View/ClientView.fxml").toUri().toURL();
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("CEYMChat");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
