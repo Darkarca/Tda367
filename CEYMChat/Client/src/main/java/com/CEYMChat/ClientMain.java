a0d642faec3faf9ed3a52c756a8bdb55ca8f1fa3
ostcarl <ostcarl@student.chalmers.se>
2018-10-01 15:24:48 +0200

    Fixed client launch on all machines

--- CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
+++ CEYMChat/Client/src/main/java/com/CEYMChat/ClientMain.java
@@ -21,7 +21,7 @@ public class ClientMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         URL url = Paths.get("Client/src/main/resources/View/ClientView.fxml").toUri().toURL();
-        Parent root = FXMLLoader.load(url); root = FXMLLoader.load(getClass().getClassLoader().getResource("View/ClientView.fxml"));
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("CEYMChat");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
