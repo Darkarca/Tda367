1f60c66b554a525c2ac4faf651afb7a61eae9b95
mhd85almasri <34286732+mhd85almasri@users.noreply.github.com>
2018-10-21 07:05:09 +0200

    get the sound format method

--- CEYMChat/model-lib/src/main/java/com/CEYMChat/AudioMessage.java
+++ CEYMChat/model-lib/src/main/java/com/CEYMChat/AudioMessage.java
@@ -1,11 +1,27 @@
 package com.CEYMChat;
 import javax.sound.sampled.AudioFormat;
 
-
+/**
+ * The Class that Describe The audio format of the file bring sent and recieved
+ */
 public class AudioMessage {
     float sRate;
     int sInbits;
     int channels;
     boolean signed;
     boolean bigEndian;
+
+
+    /**
+     * get the sound file format
+     * @return AudioFormat Type
+     */
+    public AudioFormat getAudioFormat() {
+        sRate = 16000.0F;
+        sInbits = 16;
+        channels = 1;
+        signed = true;
+        bigEndian = false;
+        return new AudioFormat(sRate, sInbits, channels, signed, bigEndian);
+    }
 }
