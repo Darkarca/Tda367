6cda58af828a1abf3ec78097bf007cb3542e06d9
mhd85almasri <34286732+mhd85almasri@users.noreply.github.com>
2018-10-21 06:57:04 +0200

    VoiceFormatInstanceVariables

---
+++ CEYMChat/model-lib/src/main/java/com/CEYMChat/AudioMessage.java
@@ -0,0 +1,11 @@
+package com.CEYMChat;
+import javax.sound.sampled.AudioFormat;
+
+
+public class AudioMessage {
+    float sRate;
+    int sInbits;
+    int channels;
+    boolean signed;
+    boolean bigEndian;
+}
