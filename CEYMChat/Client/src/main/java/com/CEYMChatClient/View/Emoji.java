fc5288b5a152c6ab99c2dbd9a64f405b9de3d3bb
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2018-10-22 22:26:26 +0200

    edited emojis json

--- CEYMChat/Client/src/main/java/com/CEYMChatClient/View/Emoji.java
+++ CEYMChat/Client/src/main/java/com/CEYMChatClient/View/Emoji.java
@@ -1,9 +1,11 @@
 package com.CEYMChatClient.View;
 
+import java.util.ArrayList;
+
 public class Emoji {
 
     String emojiChar;
     String emojiCode;
     String discription;
-    String [] tags;
+    ArrayList<String> tags;
 }
