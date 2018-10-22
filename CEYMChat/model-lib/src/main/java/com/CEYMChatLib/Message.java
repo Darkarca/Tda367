669cb79f58895be314c7573e0ee66354c524cdad
ostcarl <ostcarl@student.chalmers.se>
2018-10-22 18:01:21 +0200

    Updated comment in Message

--- CEYMChat/model-lib/src/main/java/com/CEYMChatLib/Message.java
+++ CEYMChat/model-lib/src/main/java/com/CEYMChatLib/Message.java
@@ -2,7 +2,10 @@ package com.CEYMChatLib;
 
 import java.io.Serializable;
 
-/** Generic class that works in conjunction with MessageFactory to create messages that are sent and received by the server/client. */
+/** Generic class that works in conjunction with MessageFactory to create messages that are sent and received by the server/client.
+ *  It has a package private constructor in order to ensure the generic variable "data" is not assigned something the model cannot interpret.
+ *  The factory ensures we can only create Message of predefined types.
+ * */
 
 public class Message<T> implements Serializable {
     private String sender;
