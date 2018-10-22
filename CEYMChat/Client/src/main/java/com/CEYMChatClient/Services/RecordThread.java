b1f33ed65d29adad1ced4953b467e5d0203ffdef
mhd85almasri <34286732+mhd85almasri@users.noreply.github.com>
2018-10-22 17:27:10 +0200

    the run method to record audio

--- CEYMChat/Client/src/main/java/com/CEYMChatClient/Services/RecordThread.java
+++ CEYMChat/Client/src/main/java/com/CEYMChatClient/Services/RecordThread.java
@@ -1,15 +1,19 @@
 package com.CEYMChatClient.Services;
-
 import com.CEYMChatClient.Controller.IController;
-
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.TargetDataLine;
 import java.io.File;
+import java.io.IOException;
 
+/**
+ * This class implements the Runnable to record audio while chatting
+ */
 public class RecordThread implements Runnable{
 
     // Filepath
-    File wavFile = new File("Client/Messages/RAudio.wav");
+    File directory = new File("Client/Messages/RAudio.wav");
 
     // determine the audio format
     AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
@@ -22,16 +26,20 @@ public class RecordThread implements Runnable{
     // to decide when to stop recording
     boolean stopRecord;
 
-
-
-
-
-
-
-
-
+    /**
+     * This method makes an audio stream from the mic and write it to a specified file directory
+     */
     @Override
     public void run() {
+        try {
+            System.out.println("Capturing Audio...");
+            AudioInputStream ais = new AudioInputStream(mic);
 
+            // start recording
+            System.out.println("Recording...");
+            AudioSystem.write(ais, fileType, directory);
+        }  catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
