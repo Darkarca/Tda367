7d29c1f4f7e1113bbec5066a38f2a57ea9d1b1c6
Mohamad Almasri <mhd85almasri@gmail.com>
2019-08-15 00:16:21 +0200

    A new interface that specify the operations on a sound file

---
+++ CEYMChat/Client/src/main/java/com/CEYMChatClient/Services/IVoice.java
@@ -0,0 +1,13 @@
+package com.CEYMChatClient.Services;
+
+import java.io.IOException;
+
+public interface IVoice {
+
+    public void recordVoice();
+
+    public void stopRecording();
+
+    public void playBack();
+
+}
