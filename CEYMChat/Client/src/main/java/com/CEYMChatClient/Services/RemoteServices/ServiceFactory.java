5b89a0daf66ddca464302a79a3e1f095aa02253f
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2019-08-18 14:58:55 +0200

    worked on server-path changing exception handling so if the new path is wrong, the user can give a new path within next start

--- CEYMChat/Client/src/main/java/com/CEYMChatClient/Services/RemoteServices/ServiceFactory.java
+++ CEYMChat/Client/src/main/java/com/CEYMChatClient/Services/RemoteServices/ServiceFactory.java
@@ -3,6 +3,7 @@ package com.CEYMChatClient.Services.RemoteServices;
 import com.CEYMChatClient.Model.ClientModel;
 import com.CEYMChatClient.Services.FileServices.Configurations;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -16,7 +17,8 @@ public class ServiceFactory implements IServiceFactory {
             socket = new Socket(Configurations.getInstance().getConfig().getProperty("serverPath"), 9000);
         } catch (Exception ex){
             System.out.println("Fatal Error: The Server is not started!");
-            ex.printStackTrace();
+            String serverIp = (String) JOptionPane.showInputDialog("The entered Server path is not available or wrong. Please enter a new server path (for example: localhost) and start again");
+            Configurations.getInstance().getConfig().setProperty("serverPath", serverIp);
             System.exit(0);
         }
     }
@@ -25,7 +27,7 @@ public class ServiceFactory implements IServiceFactory {
         try {
             socket = new Socket(hostname, 9000);
         } catch (IOException e) {
-            System.out.println("Could not initialize the serverSocket, exiting...");
+            System.out.println("Could not initialize the serverSocket");
             System.exit(1);
         }
     }
